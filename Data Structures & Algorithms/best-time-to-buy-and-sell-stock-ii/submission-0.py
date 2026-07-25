29b2ad3e1ba1526b3964afd0776a22aedcffd28c
AkhilS234 <akhil.vishnubhotla1@gmail.com>
2026-07-25 08:18:37 -0400

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        leftPtr = 0
+        rightPtr = 1
+        total = 0
+
+        while ((leftPtr < rightPtr) and (rightPtr < len(prices))):
+            leftPrice = prices[leftPtr]
+            rightPrice = prices[rightPtr]
+
+            if (leftPrice < rightPrice):
+                total += (rightPrice - leftPrice)
+
+            leftPtr += 1
+            rightPtr += 1
+
+        return total
